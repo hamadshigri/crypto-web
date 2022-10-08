ca452dd9528ed2f271c50afb173a143d06675d8f
hamadshigri <hamad.shigri95@gmail.com>
2022-10-08 21:33:04 +0500

    Crypto API Using React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
 import logo from './logo.svg';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+
+  const [coins, setCoins] = useState([])
+  useEffect(() => {
+    axios
+     .get(
+       'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false'
+     ).then(res => {
+       setCoins(res.data);
+     }).catch(error => console.log(error));
+  });
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {coins.map(coin => {
+        return (
+          <div>
+            <table style= {{border: 2 + "px black solid"}}>
+              <tr>
+                <td><img src={coin.image} style={{width: 20 + "%"}}></img></td>
+                <td>{coin.symbol}</td>
+                <td>{coin.current_price}</td>
+              </tr>
+            </table>
+          </div>
+        );
+      })}
     </div>
   );
 }
